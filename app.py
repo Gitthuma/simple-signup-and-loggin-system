6e700eeb6f9e5d9d369cad820444ea052d3bed6e
Gitthuma <george.githuma@gmail.com>
2021-11-20 23:28:18 +0300

    Ask for user input and check if it matches the login credentials, if yes print a scucces message if not print a invalid credential message

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ passWord = input("Please enter password: ")
 
 print("Congratulations! You have created a new account.")
 print("Please log in:")
+
+# Ask for user input and check if it matches the login credentials, if yes print a scucces message if not print a invalid credential message
+
+userNameInput = input("Please enter your user name: ")
+passWordInput = input("Plese enter your password: ")
+
+if userNameInput == userName and passWordInput == passWord:
+    print("Welcome! You have successifully logged in.")
+else:
+    print("Sorry! You have entered invalid credentials.")
