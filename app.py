195aa6a8758675179c9981b4d3019478968da6e4
Gitthuma <george.githuma@gmail.com>
2021-11-20 23:16:12 +0300

    Create welcome message that asks to create account and takes user input

---
+++ app.py
@@ -0,0 +1,6 @@
+# Create welcome message that asks to create account and takes user input
+
+print("Welcome! Please create an account:")
+
+userName = input("Please enter username: ")
+passWord = input("Please enter password: ")
